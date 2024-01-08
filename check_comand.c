b3e258ecfa035bb585f71d16bbd1ff48410277db
Marcos Zea moreno <mzea-mor@c2r9s4.42malaga.com>
2024-01-08 16:46:02 +0100

    add null entry

--- check_comand.c
+++ check_comand.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_comand.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mzea-mor <mzea-mor@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 15:46:14 by mzea-mor          #+#    #+#             */
-/*   Updated: 2023/12/27 16:15:23 by vkatason         ###   ########.fr       */
+/*   Updated: 2024/01/08 16:36:09 by mzea-mor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,26 @@
 /**
  * @brief Function used to detect if a command is valid.
  * 
- * @param ptr: a pointer that contain the string of readline.
+ * @param usr_input: a pointer that contain the string of readline.
  * @return int: "1" if ok, "0" if error.
  */
-int	check_comand(char *ptr)
+int	check_comand(char *usr_input)
 {
-	if (!ft_strncmp("echo", ptr, 5))
+	if (!ft_strncmp("echo", usr_input, 5))
 		return (1);
-	else if (!ft_strncmp("cd", ptr, 3))
+	else if (!ft_strncmp("cd", usr_input, 3))
 		return (1);
-	else if (!ft_strncmp("pwd", ptr, 4))
+	else if (!ft_strncmp("pwd", usr_input, 4))
 		return (1);
-	else if (!ft_strncmp("export", ptr, 7))
+	else if (!ft_strncmp("export", usr_input, 7))
 		return (1);
-	else if (!ft_strncmp("unset", ptr, 6))
+	else if (!ft_strncmp("unset", usr_input, 6))
 		return (1);
-	else if (!ft_strncmp("env", ptr, 4))
+	else if (!ft_strncmp("env", usr_input, 4))
 		return (1);
-	else if (!ft_strncmp("exit", ptr, 5))
+	else if (!ft_strncmp("exit", usr_input, 5))
+		return (1);
+	else if (!ft_strncmp("", usr_input, 1))
 		return (1);
 	else
 		return (0);
