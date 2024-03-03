7c5d1562a57775ac4ddba5e2e0700b36f953754f
vkatasonov <vladimir.katasonov@gmail.com>
2024-03-04 00:23:53 +0100

    Update Makefile and library functions

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 15:10:23 by vkatason          #+#    #+#             */
-/*   Updated: 2024/02/11 15:39:59 by vkatason         ###   ########.fr       */
+/*   Updated: 2024/03/03 22:37:22 by vkatason         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,7 @@ int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_calloc(size_t count, size_t size);
 char				*ft_strdup(char *s1);
 char				*ft_strcat(char *dest, char *src);
+char				*ft_strncat(char *s1, const char *s2, size_t n);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vkatason <vkatason@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 15:45:41 by mzea-mor          #+#    #+#             */
-/*   Updated: 2024/03/01 15:41:56 by vkatason         ###   ########.fr       */
+/*   Updated: 2024/03/04 00:18:28 by vkatason         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,8 @@ int	get_promp(t_data *data, char **env)
 	}
 	add_history(usr_input);
 	ft_is_input_error(usr_input);
-	ft_print_vars(ft_fill_values(usr_input, data));
+	//ft_print_vars(ft_fill_values(usr_input, data));
+	ft_printf("%s\n", data->input_copy);
 	ft_parse_input(data, usr_input);
 	if (data->token && check_builtin(data->token) == 0)
 		check_execve(data, env, data->env_copy);
